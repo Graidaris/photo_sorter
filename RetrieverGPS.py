9b9c66c23e92413162c759bcb6d056246e9f49e8
Graidaris <vladislavby96@gmail.com>
2019-05-15 20:14:47 +0200

    update

--- RetrieverGPS.py
+++ RetrieverGPS.py
@@ -16,16 +16,34 @@ class RetrieverGPS:
         return result
 
     def extract_coord(self, path):
-        image = Image.open(path)
+        gps_data_exif_IOS = 34853
+        latitude = 2
+        longitude = 4
+        pos_cord_dir_one = 1
+        pos_cord_dir_two = 3        
+        
         try:
-            n_coordinat = image._getexif()[34853][2]
-            e_coordinat = image._getexif()[34853][4]
+            image = Image.open(path)
+            n_coordinat = image._getexif()[gps_data_exif_IOS][latitude]
+            e_coordinat = image._getexif()[gps_data_exif_IOS][longitude]
+
+            cardinal_direction = (
+                image._getexif()[gps_data_exif_IOS][pos_cord_dir_one],
+                image._getexif()[gps_data_exif_IOS][pos_cord_dir_two]
+            )
+
         except KeyError:
             return None
-        except Exception:
+        except FileNotFoundError:
+            print(f"File {path} not found")
+            return None
+        except TypeError:
+            print(f"File {path} is not subscriptable")
+            return None
+        except Exception as e:
             return None
 
         cor_n = ' '.join(self.div_str(c, div) for (c, div) in n_coordinat)
         cor_e = ' '.join(self.div_str(c, div) for (c, div) in e_coordinat)
 
-        return (cor_n, 'N', cor_e, 'E')
+        return (cor_n, cardinal_direction[0], cor_e, cardinal_direction[1])
