231afbc2a2aaed3a695e8144e7489bf10e7d5700
Graidaris <vladislavby96@gmail.com>
2019-05-15 19:30:23 +0200

    update

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-from PIL import Image
-from PIL.ExifTags import TAGS, GPSTAGS
+from os.path import isfile
+from os.path import join as path_join
+from os import listdir
+
+from RetrieverGPS import RetrieverGPS
+
+path = 'OneDrive-2019-05-15'
+
+retriver_inform = RetrieverGPS()
+
+count = 0
+count_correct_processed = 0
+count_error_processed = 0
+count_not_correct_format = 0
+
+with open('coordinats.txt', 'w') as file_to_write:
+
+    for f in listdir(path):
+        count += 1
+        if f.split('.')[-1] == 'jpg':
+            cord = retriver_inform.extract_coord(path_join(path, f))
+            if cord is not None:
+                count_correct_processed += 1
+                file_to_write.write(str(cord) + '\n')
+            else:
+                count_error_processed += 1
+        else:
+            count_not_correct_format += 1
 
 
-im = Image.open('test3.jpg')
-n_coordinat = im._getexif()[34853][2]
-e_coordinat = im._getexif()[34853][4]
-
-def div_str(a, b):
-    dividend = int(a)
-    divider = int(b)
-    result = str(dividend/divider)
-    result = result if int(result.split('.')[1]) > 0 else result.split('.')[0]
-    return result
-    
-
-cor_n = ' '.join(div_str(c, div) for (c, div) in n_coordinat)
-cor_e = ' '.join(div_str(c, div) for (c, div) in e_coordinat)
-
-
-print(cor_n,'N',cor_e,'E')
+print('Complite!\n'
+      f'All file is processed: {count}\n'
+      f"Files has benn processed correct: {count_correct_processed}\n"
+      f"Files has been processed with error: {count_error_processed}\n"
+      f"Files has been not correct format: {count_not_correct_format}\n"
+      )
