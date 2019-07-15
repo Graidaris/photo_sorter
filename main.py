4bf2ab0d2def7b6b0c9e2f6b47de5df25855ef59
Graidaris <vladislavby96@gmail.com>
2019-07-15 20:42:49 +0300

    add new methods and update interface

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
 import sys
+import os
 from PySide2.QtWidgets import QApplication, QMainWindow, QPlainTextEdit
 from PySide2.QtCore import QFile
 from interface.main_window import Ui_MainWindow
+from PyQt5.QtCore import pyqtSlot, QThread, pyqtSignal
 
 from sorter import Sorter
-from log_sorter import LogSorter
 
-import _thread
+class Log:
+        def __init__(self, signal):
+            self.signal = signal
+
+        def addLog(self, text):
+            self.signal.emit(text)
+
+class SortThread(QThread):
+    signal_log = pyqtSignal(str)
+    
+    def __init__(self):
+        QThread.__init__(self)
+        self.sorter = Sorter()
+        loger = Log(self.signal_log)
+        self.sorter.setLog(loger)
+        self.api_key = None
+        self.path = None
+
+    def __del__(self):
+        self.wait()
+        
+    def stop(self):
+        self.sorter.stop()
+        self.wait()
+        
+    def setApiKey(self, key):
+        self.sorter.setKeyAPI(key)
+        
+    def setPath(self, path):
+        self.path = path
+
+    def run(self):
+        self.sorter.sort_files(self.path)        
+    
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,25 +49,80 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
-        self.log = LogSorter()
-        self.log.setWidgetLog(self.ui.plainTextEdit_logi)
-        
-        self.sorter = Sorter()
-        self.sorter.setLog(self.log)
-        
+        self.start = False
+
         self.ui.pushButton_Start.clicked.connect(self.startSort)
+        self.ui.pushButton_Stop.clicked.connect(self.stopSort)
+        self.ui.pushButton_dialog.clicked.connect(self.openDialog)
+        self.ui.progressBar.setValue(0)
+        self.ui.progressBar.hide()
+        self.sorter = SortThread()
+        self.sorter.signal_log.connect(self.addLog)
         
+    def openDialog(self):
+        self.ui.openFileNameDialog()
+        
+    def isStarted(self):
+        self.switchMode()
+        return self.start
+    
+    def disableUI(self, my_bool):
+        self.ui.plainTextEdit_pathDir.setDisabled(my_bool)
+        self.ui.plainTextEdit.setDisabled(my_bool)
+        self.ui.progressBar.setDisabled(not my_bool)
+        self.ui.pushButton_Start.setDisabled(my_bool)        
+        self.ui.pushButton_Stop.setDisabled(not my_bool)
+        self.ui.pushButton_dialog.setDisabled(my_bool)   
+        
+        if my_bool:
+            self.ui.pushButton_Start.hide()        
+            self.ui.pushButton_Stop.show()
+            self.ui.progressBar.show()
+        else:
+            self.ui.pushButton_Start.show()        
+            self.ui.pushButton_Stop.hide()
+            self.ui.progressBar.hide()
+
+    def switchMode(self):
+        if self.start:
+            self.start = False
+        else:
+            self.start = True
+            
+        self.disableUI(self.start)
+
+    def getAmountElements(self, dir_name):
+        try:
+            amount = len(os.listdir(dir_name))
+            return amount
+        except FileNotFoundError as ef:
+            self.log.addLog(ef)
+            return None
+        
+    def addLog(self, text):
+        current_value = self.ui.progressBar.value()
+        self.ui.progressBar.setValue(current_value + 1)
+        self.ui.plainTextEdit_logi.insertPlainText(text + '\n')
+
     def startSort(self):
         api_key = self.ui.plainTextEdit.toPlainText()
         dir_name = self.ui.plainTextEdit_pathDir.toPlainText()
-        
-        self.sorter.setKeyAPI(api_key)
-        test_request = self.sorter.test_request()
+        self.sorter.setApiKey(api_key)
+        test_request = Sorter.test_request(api_key)
+
         if test_request['code'] == 401:
-            self.log.addLog(test_request['message'])
-        else:
-            #self.sorter.sort_files(dir_name)# fix thread
-            _thread.start_new_thread(self.sorter.sort_files, (dir_name, ))
+            self.addLog(test_request['message'])
+        elif self.isStarted():
+            amount_elements = self.getAmountElements(dir_name)
+            self.ui.progressBar.setMaximum(amount_elements)
+            self.sorter.setPath(self.ui.plainTextEdit_pathDir.toPlainText())
+            self.sorter.start()
+            
+    def stopSort(self):
+        self.switchMode()
+        self.sorter.stop()
+        self.ui.progressBar.setValue(0)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- sorter.py
+++ sorter.py
@@ -5,7 +5,6 @@ from os.path import join
 import sys
 import requests
 import time
-import privat
 
 from retriever_photo_information import RetrieverPhotoInformation, NotPhotoType, HasntGPSData
 
@@ -14,24 +13,30 @@ I use 'opencagedata' service
 My accesses key for API of the service
 '''
 
-class LogSorter:
+
+class Log:
     def __init__(self):
         pass
-    
+
     def addLog(self, text):
         print(text)
 
+
 class Sorter:
     def __init__(self):
-        self.__log = LogSorter()
-        self.__api_key = privat.key
-    
+        self.__log = Log()
+        self.__api_key = None
+        self.stoped = False
+
     def setLog(self, log):
         self.__log = log
         
+    def stop(self):
+        self.stoped = True
+
     def setKeyAPI(self, api_key):
         self.__api_key = api_key
-    
+
     def get_location(self, lat, lon):
         if not lat or not lon:
             return None
@@ -50,66 +55,68 @@ class Sorter:
             location['country'] = request.json()['results'][0]['components']['country']
         except KeyError:
             location['country'] = None
-        
+
         try:
             location['city'] = request.json()['results'][0]['components']['city']
         except KeyError:
             location['city'] = None
 
         return location
-        
-    def test_request(self):
+
+    @staticmethod
+    def test_request(api):
         URL = 'https://api.opencagedata.com/geocode/v1/json'
         #test request, London latlon
         params = {
             "q": str(51.528583) + ' ' + str(-0.192254),
-            "key": self.__api_key
+            "key": api
         }
         request = requests.get(url=URL, params=params)
         return request.json()["status"]
-    
+
     def sort_files(self, path):
         if not os.path.isdir(path):
             return None
-        
+
         if self.__api_key is None:
             self.__log.addLog("You forgot put api key.")
             return None
         
+        self.stoped = False
+
         for photo in os.listdir(path):
-            if not os.path.isfile(join(path,photo)):
-                continue
+            if self.stoped:
+                self.__log.addLog("Sorter has been stoped...")
+                return
             
+            if not os.path.isfile(join(path, photo)):
+                continue
+
             try:
                 photo_exif_info = RetrieverPhotoInformation(join(path, photo))
-                coord = photo_exif_info.get_coordinates()            
-                location = self.get_location(coord['lat'], coord['lon'])            
-                target_dir = ""            
+                coord = photo_exif_info.get_coordinates()
+                location = self.get_location(coord['lat'], coord['lon'])
+                target_dir = ""
                 target_dir = join(path, location['country'])
-                
+
                 if not os.path.exists(target_dir):
                     os.mkdir(target_dir)
-                
+
                 if location['city'] is not None:
                     target_dir = join(target_dir, location['city'])
                     if not os.path.exists(target_dir):
                         os.mkdir(target_dir)
-                        
+
                 os.rename(join(path, photo), join(target_dir, photo))
-                self.__log.addLog(join(path, photo) + " has change name to " + join(target_dir, photo))
-                    
+                self.__log.addLog(
+                    join(path, photo) + " has change name to " + join(target_dir, photo)
+                )
+
             except HasntGPSData:
                 self.__log.addLog(f"{photo} hasnt GPS data")
             except NotPhotoType:
                 self.__log.addLog(f"File {photo} is not a photo")
             except TypeError:
                 self.__log.addLog(f"Type error {photo}")
-                
+
         self.__log.addLog("Done...\n\n\n")
-
-
-if __name__ == '__main__':
-    path = privat.path
-    sorter = Sorter()
-    sorter.sort_files(path)
-    
